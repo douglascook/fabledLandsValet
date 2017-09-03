b4f485ff2791d6d6cb7c4241565b5bb5e588c2a1
Douglas Cook <douglas.cook@eigentech.com>
2017-09-03 19:05:49 +0100

    Correctly update character store with item stats

--- app/character/reducer.js
+++ app/character/reducer.js
@@ -5,7 +5,7 @@ import { initialState } from '../reducer.js';
 export default function character(state = initialState.stats, action) {
   switch (action.type) {
     case ADD_ITEM:
-      return [...state, updateStatModifier(state, action)];
+      return updateStatModifier(state, action);
 
     default:
       return state;
@@ -15,8 +15,8 @@ export default function character(state = initialState.stats, action) {
 function updateStatModifier(state, action) {
   // TODO handle multiple effects
   const itemEffect = action.item.effects[0];
-  const stat = state.filter(s => s.name === itemEffect.skill)[0];
-  const statModifier = stat.modifier || 0;
-  stat.modifier = statModifier + itemEffect.value;
-  return stat;
+  const index = state.findIndex(s => s.name === itemEffect.skill);
+  const newState = [...state];
+  newState[index].modifier = (state[index].modifier || 0) + itemEffect.value;
+  return newState;
 }
