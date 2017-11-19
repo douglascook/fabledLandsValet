921c621c1b4795802c01a19772acd27a1c7be783
Douglas Cook <douglas.cook@eigentech.com>
2017-11-19 18:04:43 +0000

    Fix submit button export

--- app/shared/components/index.js
+++ app/shared/components/index.js
@@ -5,3 +5,5 @@ export { default as ItemPicker } from './itemPicker';
 export { default as RemovableRow } from './removableRow';
 
 export { default as SingleItemRow } from './singleItemRow';
+
+export { default as SubmitButtonRow } from './submitButtonRow.js';
