36c7afa5c8c07d63e426d0a1efd6861554b44004
Douglas Cook <douglas.cook@eigentech.com>
2018-03-04 21:52:53 +0000

    Icons!
    Update react-navigation

--- app/navigation.js
+++ app/navigation.js
@@ -1,6 +1,11 @@
+import React from 'react';
+
 import {
   TabNavigator,
 } from 'react-navigation';
+
+import FaIcon from 'react-native-vector-icons/FontAwesome';
+import MatIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Character from './character/character';
 import Possessions from './possessions/possessions';
@@ -9,48 +14,55 @@ import Codewords from './codewords/codewords';
 import Tickboxes from './tickboxes/tickboxes';
 import Settings from './settings/settings';
 
+
 const Navigation = TabNavigator({
   character: {
     screen: Character,
     navigationOptions: {
-      tabBarLabel: 'Character'
+      tabBarIcon: () => <FaIcon name="user-circle-o" size={26} color="white" />,
     },
   },
   possessions: {
     screen: Possessions,
     navigationOptions: {
-      tabBarLabel: 'Possessions'
+      tabBarIcon: () => <MatIcon name="sword" size={26} color="white" />,
     },
   },
   ships: {
     screen: Ships,
     navigationOptions: {
-      tabBarLabel: 'Ships'
+      tabBarIcon: () => <MatIcon name="ship-wheel" size={26} color="white" />,
     },
   },
   codewords: {
     screen: Codewords,
     navigationOptions: {
-      tabBarLabel: 'Codewords'
+      tabBarIcon: () => <MatIcon name="playlist-check" size={26} color="white" />,
     },
   },
   tickboxes: {
     screen: Tickboxes,
     navigationOptions: {
-      tabBarLabel: 'Tickboxes'
+      tabBarIcon: () => <MatIcon name="check-all" size={26} color="white" />,
     },
   },
   settings: {
     screen: Settings,
     navigationOptions: {
-      tabBarLabel: 'Settings'
+      tabBarIcon: () => <MatIcon name="settings" size={26} color="white" />,
     },
   },
 }, {
   tabBarPosition: 'bottom',
   lazyLoad: true,
   tabBarOptions: {
+    showLabel: false,
+    showIcon: true,
     upperCaseLabel: false,
+    iconStyle: {
+      width: 30,
+      height: 30,
+    },
   }
 });
 
