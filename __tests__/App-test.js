89d61b6ab658a3e9d5682b4af76c5886174bfca0
Douglas Cook <douglas@kheironmed.com>
2019-04-13 12:43:49 +0100

    Get basic app test running

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,6 +9,9 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+// https://github.com/facebook/jest/issues/4359
+jest.useFakeTimers();
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
