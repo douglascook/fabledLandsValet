2c01aeb5be6365a8dddb20d3dae822910a3d9272
Douglas Cook <dugup@hotmail.co.uk>
2019-10-26 18:28:00 +0100

    Fix async issue with jest

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,7 +1,3 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
@@ -9,8 +5,6 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-// https://github.com/facebook/jest/issues/4359
-jest.useFakeTimers();
 
 it('renders correctly', () => {
   renderer.create(<App />);
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|react-native-vector-icons|react-navigation-tabs|react-native-screens|@react-navigation|react-native-safe-area-view|@react-native-community)/)'
-  ]
+  ],
+  // timers and test env config needed due to https://github.com/facebook/jest/issues/4359
+  timers: 'fake',
+  testEnvironment: 'jsdom'
 };
